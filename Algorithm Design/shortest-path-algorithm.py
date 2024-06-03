e14f200c2d207256796c3d46587a258c1a753a04
LiisaRebane <liisareb@gmail.com>
2024-06-03 18:53:26 +0300

    changed print statement

--- Algorithm Design/shortest-path-algorithm.py
+++ Algorithm Design/shortest-path-algorithm.py
@@ -5,23 +5,29 @@ my_graph = {
     'D': [('A', 1), ('C', 7)]
 }
 
-def shortest_path(graph, start, target=''):
+def shortest_path(graph, start, target = ''):
     unvisited = list(graph)
     distances = {node: 0 if node == start else float('inf') for node in graph}
     paths = {node: [] for node in graph}
     paths[start].append(start)
+    
     while unvisited:
         current = min(unvisited, key=distances.get)
         for node, distance in graph[current]:
             if distance + distances[current] < distances[node]:
                 distances[node] = distance + distances[current]
                 if paths[node] and paths[node][-1] == node:
-                    paths[node] = paths[node][:]
+                    paths[node] = paths[current][:]
                 else:
                     paths[node].extend(paths[current])
                 paths[node].append(node)
         unvisited.remove(current)
 
-    print(f'Unvisited: {unvisited}\nDistances: {distances}\nPaths: {paths}')
-
+    targets_to_print = [target] if target else graph
+    for node in targets_to_print:
+        if node == start:
+            continue
+        print(f'\n{start}-{node} distance: {distances[node]}\nPath: {" -> ".join(paths[node])}')
+    
+    return distances, paths
 shortest_path(my_graph, 'A')
